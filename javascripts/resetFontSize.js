c7502277eba576228bf8bb3198e3648f743d335e
vincentdd <scar_dante@hotmail.com>
2017-06-23 20:42:27 +0800

    IOS10 scale unable

--- javascripts/resetFontSize.js
+++ javascripts/resetFontSize.js
@@ -1,3 +1,18 @@
 $(function() {
-	document.documentElement.style.fontSize = document.documentElement.clientWidth / 7.5 + 'px';
-});
+		document.documentElement.style.fontSize = document.documentElement.clientWidth / 7.5 + 'px';
+	}
+	/*IOS10禁止缩放*/
+	document.documentElement.addEventListener('touchstart', function(event) {
+		if (event.touches.length > 1) {
+			event.preventDefault();
+		}
+	}, false);
+
+	var lastTouchEnd = 0; document.documentElement.addEventListener('touchend', function(event) {
+		var now = Date.now();
+		if (now - lastTouchEnd <= 300) {
+			event.preventDefault();
+		}
+		lastTouchEnd = now;
+	}, false);
+);
